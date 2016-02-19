fdc9cc5bbca1070e663397c344c3025ee8b7d660
Damodar Dahal <underscoredam@gmail.com>
2016-02-19 20:52:50 +0545

    Added irrKlang dependencies

--- clean.sh
+++ clean.sh
@@ -1,1 +1,10 @@
-clean.sh
+while true; do
+    read -p "Do you wish to clean Empathy? Any untracked file in git will be reomved.Y/N" yn
+    case $yn in
+        [Yy]* ) git clean -d -f -x;break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "Everything done!"
