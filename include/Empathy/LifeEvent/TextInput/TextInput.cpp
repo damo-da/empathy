9ebce492617e7800371952a6b0f3c2128708faa5
Damodar Dahal <underscoredam@gmail.com>
2017-01-17 13:17:49 -0600

    integrate backspace on text input

--- include/Empathy/LifeEvent/TextInput/TextInput.cpp
+++ include/Empathy/LifeEvent/TextInput/TextInput.cpp
@@ -14,12 +14,22 @@ void empathy::life_event::TextInput::onReceiveEvent(empathy::radio::Event &event
     if (event.action == EMPATHY_EVENT_INPUT_KEY_PRESS){
         int key = event.getInt(EMPATHY_EVENT_INPUT_KEY);
 
-        if (key == 257){
+
+        if (key == 257){//enter key
             empathy::radio::Event e(EMPATHY_TEXT_INPUT_ENTERED);
             e.putString(EMPATHY_TEXT_INPUT_TEXT, getText());
             dispatchEventToActions(EMPATHY_TEXT_INPUT_ENTERED, e);
-        }else {
-            std::string text = Text::getText() + (char)key;
+        }else{
+            //instantiate with original text
+            std::string text= Text::getText();
+
+            if (key == 259) { //back space
+                text = text.length() == 0?"":text.substr(0, text.size()-1);
+
+            } else if(key >=32 && key <= 254){//printable characters
+                text += (char)key;
+            }
+
             Text::setText(text);
         }
 
