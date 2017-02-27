3a949020e1e1b7f7a2e5d8214f0a216df0696b2b
Damodar Dahal <underscoredam@gmail.com>
2017-02-27 16:44:16 -0600

    fix never ending TextInput

--- include/Empathy/LifeEvent/TextInput/TextInput.cpp
+++ include/Empathy/LifeEvent/TextInput/TextInput.cpp
@@ -11,27 +11,31 @@ empathy::life_event::TextInput::TextInput() : Text(){
 void empathy::life_event::TextInput::onReceiveEvent(empathy::radio::Event &event) {
     Text::onReceiveEvent(event);
 
-    if (event.action == EMPATHY_EVENT_INPUT_KEY_PRESS){
-        int key = event.getInt(EMPATHY_EVENT_INPUT_KEY);
+    if(isRunning()){
+
+        if (event.action == EMPATHY_EVENT_INPUT_KEY_PRESS){
+            int key = event.getInt(EMPATHY_EVENT_INPUT_KEY);
 
 
-        if (key == 257){//enter key
-            empathy::radio::Event e(EMPATHY_TEXT_INPUT_ENTERED);
-            e.putString(EMPATHY_TEXT_INPUT_TEXT, getText());
-            dispatchEventToActions(EMPATHY_TEXT_INPUT_ENTERED, e);
-        }else{
-            //instantiate with original text
-            std::string text= Text::getText();
+            if (key == 257){//enter key
+                empathy::radio::Event e(EMPATHY_TEXT_INPUT_ENTERED);
+                e.putString(EMPATHY_TEXT_INPUT_TEXT, getText());
+                dispatchEventToActions(EMPATHY_TEXT_INPUT_ENTERED, e);
+            }else{
+                //instantiate with original text
+                std::string text= Text::getText();
 
-            if (key == 259) { //back space
-                text = text.length() == 0?"":text.substr(0, text.size()-1);
+                if (key == 259) { //back space
+                    text = text.length() == 0?"":text.substr(0, text.size()-1);
 
-            } else if(key >=32 && key <= 254){//printable characters
-                text += (char)key;
+                } else if(key >=32 && key <= 254){//printable characters
+                    text += (char)key;
+                }
+
+                Text::setText(text);
             }
 
-            Text::setText(text);
         }
-
     }
+
 }
